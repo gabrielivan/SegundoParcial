ba6638014909750d37debf27ced33e960d4e781b
gabrielivan <40808771+gabrielivan@users.noreply.github.com>
2018-11-26 23:41:54 -0300

    Add files via upload

---
+++ ParcialDos/ParcialDos/Controller.h
@@ -0,0 +1,9 @@
+
+int controller_loadFromText(char* path , LinkedList* listaVenta);
+int controller_saveAsText(char* path , LinkedList* listaVenta);
+int controller_ListarEmpleados(LinkedList* listaVentas);
+
+
+
+
+
---
+++ ParcialDos/ParcialDos/informes.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "LinkedList.h"
+#include "Venta.h"
+#include "utn.h"
+
+int informe_cantidadTotal(LinkedList* listaVentas);
+int informe_cantidadMontoMayor(LinkedList* listaVentas);
+int informe_cantidadMontoMayorMas(LinkedList* listaVentas);
+int informe_cantidadLcdTV(LinkedList* listaVentas);
